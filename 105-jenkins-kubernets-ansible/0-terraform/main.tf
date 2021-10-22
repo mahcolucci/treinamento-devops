b464dd3b5bae828488c2a8279fcc6d18ae149c1f
Marcela Colucci <marcela.colucci@hotmail.com>
2021-10-22 15:01:04 +0000

    config jenkins

--- 105-jenkins-kubernets-ansible/0-terraform/main.tf
+++ 105-jenkins-kubernets-ansible/0-terraform/main.tf
@@ -6,12 +6,19 @@ data "http" "myip" {
   url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
 }
 
-resource "aws_instance" "jenkins" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.large"
-  key_name      = "treinamento-turma1_itau"
+resource "aws_instance" "jenkins-colucci" {
+  ami                         = "ami-09e67e426f25ce0d7"
+  instance_type               = "t2.large"
+  key_name                    = "kp-colucci"
+  subnet_id                   = "subnet-0dbc6439c94e66d76"
+  associate_public_ip_address = true
+  root_block_device {
+    encrypted = true
+    #kms_key_id  = "arn:aws:kms:us-east-1:534566538491:key/90847cc8-47e8-4a75-8a69-2dae39f0cc0d"
+    volume_size = 20
+  }
   tags = {
-    Name = "jenkins"
+    Name = "jenkins-colucci"
   }
   vpc_security_group_ids = ["${aws_security_group.jenkins.id}"]
 }
@@ -19,6 +26,7 @@ resource "aws_instance" "jenkins" {
 resource "aws_security_group" "jenkins" {
   name        = "acessos_jenkins"
   description = "acessos_jenkins inbound traffic"
+  vpc_id      = "vpc-000ac43d9700f2e6c"
 
   ingress = [
     {
@@ -26,7 +34,7 @@ resource "aws_security_group" "jenkins" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids  = null,
       security_groups : null,
@@ -37,7 +45,7 @@ resource "aws_security_group" "jenkins" {
       from_port        = 8080
       to_port          = 8080
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids  = null,
       security_groups : null,
@@ -60,7 +68,7 @@ resource "aws_security_group" "jenkins" {
   ]
 
   tags = {
-    Name = "jenkins-lab"
+    Name = "jenkins-lab-colucci"
   }
 }
 
@@ -68,10 +76,10 @@ resource "aws_security_group" "jenkins" {
 output "jenkins" {
   value = [
     "jenkins",
-    "id: ${aws_instance.jenkins.id}",
-    "private: ${aws_instance.jenkins.private_ip}",
-    "public: ${aws_instance.jenkins.public_ip}",
-    "public_dns: ${aws_instance.jenkins.public_dns}",
-    "ssh -i ~/Desktop/devops/treinamentoItau ubuntu@${aws_instance.jenkins.public_dns}"
+    "id: ${aws_instance.jenkins-colucci.id}",
+    "private: ${aws_instance.jenkins-colucci.private_ip}",
+    "public: ${aws_instance.jenkins-colucci.public_ip}",
+    "public_dns: ${aws_instance.jenkins-colucci.public_dns}",
+    "ssh -i ~/.ssh/id_rsa ubuntu@${aws_instance.jenkins-colucci.public_dns}"
   ]
 }
--- 105-jenkins-kubernets-ansible/deploy.sh
+++ 105-jenkins-kubernets-ansible/deploy.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 cd 0-terraform
-~/terraform/terraform init
-~/terraform/terraform fmt
-~/terraform/terraform apply -auto-approve
+~/terraform init
+~/terraform fmt
+~/terraform apply -auto-approve
 
 echo $"[ec2-jenkins]" > ../1-ansible/hosts # cria arquivo
-echo "$(~/terraform/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../1-ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(~/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../1-ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 30 # 30 segundos
 
 cd ../1-ansible
-ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/Desktop/devops/treinamentoItau
+ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/id_rsa
 
 cd ../0-terraform
-~/terraform/terraform output
+~/terraform output
 
-echo $"Agora somente abrir a URL: http://$(~/terraform/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
+echo $"Agora somente abrir a URL: http://$(~/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
 
-ID_MAQUINA=$(~/terraform/terraform output | grep id | awk '{print $2;exit}')
+ID_MAQUINA=$(~/terraform output | grep id | awk '{print $2;exit}')
 echo ${ID_MAQUINA/\",/}
 
 # cd ../2-terraform-ami/
