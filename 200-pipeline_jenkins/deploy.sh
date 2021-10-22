3ac7120ddcd5e7732a93346913e60acc64ed2acf
Marcela Colucci <marcela.colucci@hotmail.com>
2021-10-22 18:38:10 +0000

    alterando deploy

--- 200-pipeline_jenkins/deploy.sh
+++ 200-pipeline_jenkins/deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cd 200-pipeline_jenkins/terraform
-~/terraform init
-~/terraform fmt
-~/terraform apply -auto-approve
+cd terraform
+/usr/local/bin/terraform init
+/usr/local/bin/terraform fmt
+/usr/local/bin/terraform apply -auto-approve
 
 echo "Aguardando criação de maquinas ..."
 sleep 30 # 30 segundos
 
 echo $"[ec2-pipeline-jenkins]" > ../ansible/hosts # cria arquivo
-echo "$(~/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts 
+echo "$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts 
 
 echo "Aguardando criação de maquinas ..."
 sleep 30 # 30 segundos
 
 cd ../ansible
 
-echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa ]"
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key .ssh/id_rsa ]"
 sudo ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/id_rsa
 
